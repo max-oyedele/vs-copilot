d3d1fbabb24c3f2c75e182286e3354b5ba6cf493
Segunfunmi Oyedele <eastsea1020n@gmail.com>
2024-09-17 09:10:46 -0400

    remove

--- src/ChatbotConfig.tsx
+++ src/ChatbotConfig.tsx
@@ -1,9 +1,5 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 import Options from "./widgets/Options";
-import GlobalStatistics from "./widgets/GlobalStatistics";
-import LocalStatistics from "./widgets/LocalStatistics";
-import Contact from "./widgets/Contact";
-import MedicineDelivery from "./widgets/MedicineDelivery";
 import CoBotAvatar from "./CoBotAvatar";
 
 const ChatbotConfig = {
@@ -35,31 +31,7 @@ const ChatbotConfig = {
       widgetFunc: (props: any) => <Options title="Options" {...props} />,
       mapStateToProps: ["messages"],
       props: {},
-    },
-    {
-      widgetName: "globalStatistics",
-      widgetFunc: () => <GlobalStatistics />,
-      mapStateToProps: [],
-      props: {},
-    },
-    {
-      widgetName: "localStatistics",
-      widgetFunc: () => <LocalStatistics />,
-      mapStateToProps: [],
-      props: {},
-    },
-    {
-      widgetName: "emergencyContact",
-      widgetFunc: () => <Contact />,
-      mapStateToProps: [],
-      props: {},
-    },
-    {
-      widgetName: "medicineDelivery",
-      widgetFunc: () => <MedicineDelivery />,
-      mapStateToProps: [],
-      props: {},
-    },
+    },    
   ],
 };
 
